a98c1859340900d291ab0b580c2eaffd6074e2b5
prova <prova@prova>
2020-02-21 18:01:20 +0100

    Version 1

---
+++ PiedraPapelTijeras.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class PiedraPapelTijeras
+{
+    public static void main( String[] args)
+    {
+        int R = 0;
+        int P = 1;
+        int S = 2;
+
+
+        int u;
+        int c;
+        Scanner myScan = new Scanner(System.in);
+        Random myRand = new Random();
+
+        System.out.println("Escoga una opción: ")
+        System.out.println("0. Piedra");
+        System.out.println("1. Papel");
+        System.out.println("2. Tijeras");
+
+        u = myScan.nextInt();
+
+        c = myRand.nextInt(5);
+
+
+        System.out.println("IA = " + c + " Tu = " + u);
+
+
+        if ( u==c )
+        {
+            System.out.println("Empate!");
+        }
+        else if ( (u==R && c==S) || (u==S && c==P) || (u==P && c== R) )
+        {
+            System.out.println("Has Ganado!");
+        }
+        else
+        {
+            System.out.println("Loser!");
+        }
+    }
+}
